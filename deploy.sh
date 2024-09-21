ff0134be0a7c89a480f7fa3cb18e2a3582f8e657
Martín Quesada <martinquesada@iremedy.com>
2024-09-20 18:42:06 -0700

    Debug 1

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,13 @@
 
 # Login to Kubernetes Cluster.
 if [ -n "$CLUSTER_ROLE_ARN" ]; then
+  echo "Paso 1 - Cluster ARN"
   aws eks \
     --region ${AWS_REGION} \
     update-kubeconfig --name ${CLUSTER_NAME} \
     --role-arn=${CLUSTER_ROLE_ARN}
 else
+  echo "Paso 1"
   aws eks \
     --region ${AWS_REGION} \
     update-kubeconfig --name ${CLUSTER_NAME}
